26a7384298c95a69192d0c598a137566c6f1510c
Kimmypara <kparascandalo@gmail.com>
2026-04-28 17:33:22 +0200

    arranged error

--- api/post/postsWithComments.php
+++ api/post/postsWithComments.php
@@ -11,61 +11,50 @@ include_once("../../includes/initialize.php");
 // instance of the Post class
 
 $post = new Post($db);
-$comment = new Comment($db);
+$commentObj = new Comment($db);
 
-// call the necessary function(s)
-    // read function from User
 $result = $post->read();
 $num = $result->rowCount();
 
-if($num > 0){
+if ($num > 0) {
     $posts_list = array();
     $posts_list['data'] = array();
 
-
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
-       
-        $comment->post_id = $post_id;
-        $commentsResult = $comment-> readByPostId();
 
-        $commentsNum = $commentsResult->rowCount();
-        if($commentsNum > 0){
-            $commentsList = array();
-            $commentsList["data"] = array();
+        $commentObj->post_id = $post_id;
+        $commentsResult = $commentObj->readByPostId();
 
-            while($commentRow = $commentsResult->fetch(PDO::FETCH_ASSOC)){
-                extract($commentRow);
-//  print_r($commentRow);
-                $comment_item = array(
-                    "comment_id"    => $comment_id,
-                    "comment"       => $comment,
-                    "user_id"    => $user_id,
-                    "post_id"     => $post_id,
-                    
-                );
-                array_push($commentsList['data'], $comment_item);
-        }
+        $commentsList = array();
+        $commentsList["data"] = array();
+
+        while ($commentRow = $commentsResult->fetch(PDO::FETCH_ASSOC)) {
+            $comment_item = array(
+                "comment_id" => $commentRow["comment_id"],
+                "comment"    => $commentRow["comment"],
+                "user_id"    => $commentRow["user_id"],
+                "post_id"    => $commentRow["post_id"]
+            );
+
+            array_push($commentsList['data'], $comment_item);
         }
 
         $post_item = array(
-            "post_id"    => $post_id,
-            "title"  => $title,
-            "content" => $content,
+            "post_id"  => $post_id,
+            "title"    => $title,
+            "content"  => $content,
             "user_id"  => $user_id,
-            "comments" => $commentsList,
+            "comments" => $commentsList
         );
 
         array_push($posts_list['data'], $post_item);
-
     }
 
     echo json_encode($posts_list);
+} else {
+    echo json_encode(array("message" => "No posts found."));
 }
-else{
-    echo json_encode(array("message"=>"No posts found."));
-}
-
     // readByUserId from Post
 
 
